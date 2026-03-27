a66da0079cf68a89a6bf5f49905d09edf5cea0d6
Naima <naimatnet3@gmail.com>
2026-03-27 05:16:32 +0500

    db updates

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,22 +1,22 @@
 const Pool = require("pg").Pool
 require('dotenv').config();
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: 'studentstorage-db-do-user-15873121-0.c.db.ondigitalocean.com',
-//     port: 25060,    
-//     database: "student-storage-v2",
-//     ssl: { rejectUnauthorized: false }
-// });
-
 const pool = new Pool({
-    user: 'postgres',
-    password: "rootadmin",
-    host: "localhost",
-    port: 5432,    
-    database: "student_storage_v2",
-    ssl: false
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,    
+    database: "student-storage-v2",
+    ssl: { rejectUnauthorized: false }
 });
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: "rootadmin",
+//     host: "localhost",
+//     port: 5432,    
+//     database: "student_storage_v2",
+//     ssl: false
+// });
 
 module.exports = pool;
