46a3ba6a149e26f9129f32a3a2ed86b6b9677ce5
Naima <naimatnet3@gmail.com>
2026-04-02 03:10:07 +0500

    db updated

--- backend/database/db.js
+++ backend/database/db.js
@@ -12,22 +12,22 @@ require('dotenv').config();
 
 console.log("Connecting to host:", process.env.DB_HOST);
 
-// const pool = new Pool({
-//     user: process.env.DB_USER,
-//     password: process.env.DB_PASSWORD,
-//     host: process.env.DB_HOST,
-//     port: process.env.DB_PORT || 25060, // Fallback to DO port if env fails
-//     database: process.env.DB_NAME || "student-storage-v2",
-//     ssl: { rejectUnauthorized: false }
-// });
-
 const pool = new Pool({
-    user: 'postgres',
-    password: "rootadmin",
-    host: "localhost",
-    port: 5432,    
-    database: "student_storage_v2",
-    ssl: false
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT || 25060, // Fallback to DO port if env fails
+    database: process.env.DB_NAME || "student-storage-v2",
+    ssl: { rejectUnauthorized: false }
 });
+
+// const pool = new Pool({
+//     user: 'postgres',
+//     password: "rootadmin",
+//     host: "localhost",
+//     port: 5432,    
+//     database: "student_storage_v2",
+//     ssl: false
+// });
 
 module.exports = pool;
